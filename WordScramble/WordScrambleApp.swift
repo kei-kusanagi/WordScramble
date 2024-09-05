b0942ec9ed2f7be728773b099da6573177485e2b
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-05 12:23:19 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Juan Carlos Robledo Morales on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
